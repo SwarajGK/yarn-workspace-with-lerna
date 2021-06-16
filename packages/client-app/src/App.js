96a9712b3c65be68ebf557002604793a497306ab
Swaraj Gandhi <swaraj@browserstack.com>
2021-06-16 15:16:13 +0530

    first stable version for all the changes

--- packages/client-app/src/App.js
+++ packages/client-app/src/App.js
@@ -1,12 +1,16 @@
-import { Button } from "@browserstack-demo/component-lib";
+import { Button, Input } from "@browserstack-demo/component-lib";
 
 function App() {
   const onClick = () => {
     console.log("This is a sample click");
   };
+  const onChange = (value) => {
+    console.log(value);
+  }
   return (
     <div className="App">
       <Button onClick={onClick}>Click me</Button>
+      <Input type="text" placeholder="This is the sample text" onChange={onChange} />
     </div>
   );
 }
