206442fbbd01bcf55ad37472873c29b3a0cf34bb
Swaraj Gandhi <swaraj@browserstack.com>
2021-06-16 16:33:19 +0530

    tested with older version

--- packages/client-app/src/App.js
+++ packages/client-app/src/App.js
@@ -6,11 +6,16 @@ function App() {
   };
   const onChange = (value) => {
     console.log(value);
-  }
+  };
   return (
     <div className="App">
       <Button onClick={onClick}>Click me</Button>
-      <Input type="text" placeholder="This is the sample text" onChange={onChange} />
+      <Input
+        type="text"
+        placeholder="This is the sample text"
+        onChange={onChange}
+      />
+      {/* <Link href="/">Main page</Link> */}
     </div>
   );
 }
